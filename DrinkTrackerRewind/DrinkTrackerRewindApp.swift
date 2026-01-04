e9305bb089e79f7ffb533181e7f88d14b648b38f
Toufiq Hasan <Toufiqh12@gmail.com>
2026-01-04 16:38:51 -0500

    Initial Commit

---
+++ DrinkTrackerRewind/DrinkTrackerRewindApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkTrackerRewindApp.swift
+//  DrinkTrackerRewind
+//
+//  Created by Toufiq Hasan on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct DrinkTrackerRewindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
